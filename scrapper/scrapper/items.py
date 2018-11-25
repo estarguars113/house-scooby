f4124bc511103865059c5da2373ca37094230f41
paolagc <estarguars113@gmail.com>
2018-11-25 18:09:37 -0500

    Added property type in el pais spider

--- scrapper/scrapper/items.py
+++ scrapper/scrapper/items.py
@@ -54,7 +54,7 @@ class PropertyItem(Item):
         output_processor=TakeFirst()
     )
     property_type = Field(
-        input_processor=MapCompose(strip_spaces, remove_accents),
+        input_processor=MapCompose(remove_tags, strip_spaces, remove_accents),
         output_processor=TakeFirst()
     )
     link = Field(
